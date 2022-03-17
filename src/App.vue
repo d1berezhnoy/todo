40ccf19e984843931aae3ec0c54df091d478927d
d1berezhnoy <d1m-berezhnoy@yandex.ru>
2022-03-17 22:49:22 +0400

    init: create vue app

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <GreetingPage title="ToDo Application" />
+  </main>
+</template>
+
+<script setup lang="ts">
+import GreetingPage from "@/pages/GreetingPage.vue";
+</script>
---
+++ src/pages/GreetingPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name: "greeting-page",
+  props: {
+    title: {
+      type: String,
+      default: "hello",
+    },
+  },
+};
+</script>
